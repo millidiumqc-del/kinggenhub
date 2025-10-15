7003d7fdaadfc8160a8f8e291344c6754e6b4df3
millidiumqc-del <millidiumqc@gmail.com>
2025-10-14 23:58:28 -0400

    Delete netlify/functions/hello.js

--- netlify/functions/hello.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: "Bravo! Le test a fonctionné. La structure du projet est maintenant correcte."
-  };
-};
