df8cbc59f15e4cf40b9f99fd90a55247fe1107c1
millidiumqc-del <millidiumqc@gmail.com>
2025-10-14 23:57:29 -0400

    Create hello.js

---
+++ netlify/functions/hello.js
@@ -0,0 +1,6 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: "Bravo! Le test a fonctionné. La structure du projet est maintenant correcte."
+  };
+};
